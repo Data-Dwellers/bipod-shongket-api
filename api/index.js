2114484d5e559d3909917085b08e8251b7644296
Nasif Ahmed <nasif2ahmed@gmail.com>
2025-04-20 13:50:43 +0600

    Add setup for vercel deployment

---
+++ api/index.js
@@ -0,0 +1,74 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const userRoutes = require("../routes/userRoutes");
+const morgan = require("morgan");
+
+// middlewares
+app.use(morgan("dev"));
+app.use(
+    cors({
+        // origin: [
+        //     "http://localhost:3000",
+        //     // Add deployed frontend address here
+        // ],
+        // Allow all origins for development. Use above code for production
+        origin: "*",
+    })
+);
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+
+// Routes
+app.use("/api/users", userRoutes);
+
+// Old code for non-serverless manul deploy
+// const runServer = async function () {
+//     try {
+//         await mongoose.connect(process.env.MONGODB_URI).then(() => {
+//             console.log("Database connected...");
+//         });
+//     } catch (error) {
+//         console.log("Error connecting to database:", error);
+//     }
+//     app.listen(PORT, () => {
+//         console.log(`App is running on port ${PORT}`);
+//     });
+// };
+
+// module.exports = runServer;
+
+//
+// This is a setup for deploying on vercel serverless setup
+
+// Connect to MongoDB
+let isConnected = false;
+const connectToDatabase = async () => {
+    if (isConnected) return;
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        isConnected = true;
+        console.log("Database connected...");
+    } catch (error) {
+        console.log("Error connecting to database:", error);
+        throw error;
+    }
+};
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
+});
+
+// Export handler function for Vercel
+module.exports = async (req, res) => {
+    await connectToDatabase();
+    return app(req, res);
+};
