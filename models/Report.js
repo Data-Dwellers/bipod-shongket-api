0eb91805dfc7254c35c849490458fc71e6f02b61
Nasif Ahmed <nasif2ahmed@gmail.com>
2025-04-21 12:35:37 +0600

    Fix typo

--- models/Report.js
+++ models/Report.js
@@ -13,5 +13,5 @@ const reportSchema = new mongoose.Schema({
     },
 });
 
-const Report = mongoose.model("Report", userSchema);
+const Report = mongoose.model("Report", reportSchema);
 module.exports = Report;
