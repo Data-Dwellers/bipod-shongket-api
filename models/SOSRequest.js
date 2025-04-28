ccbb2ea1003c21fc81159eb780787f417c7bfc21
Siam Sadman <sadman.bin.anwar.siam@gmail.com>
2025-04-29 02:45:25 +0600

    add user field

--- models/SOSRequest.js
+++ models/SOSRequest.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
 const sosRequestSchema = new mongoose.Schema({
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    date: { type: Date, default: Date.now },
-    location: {
-        lat: Number,
-        long: Number,
-    },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  user: { type: String },
+  date: { type: Date, default: Date.now },
+  location: {
+    lat: Number,
+    long: Number,
+  },
 });
 
 const SOSRequest = mongoose.model("SOSReqeust", sosRequestSchema);
